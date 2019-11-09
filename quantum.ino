98fb2bcac485bb0ce889ac70d1dec8a1629f34c5
gw8080 <52276914+gw8080@users.noreply.github.com>
2019-11-10 10:22:18 +1100

    Add files via upload

--- quantum.ino
+++ quantum.ino
@@ -1,10 +1,4 @@
-   /*
-Copyright (C) 2019 George Wagenknecht
-Qubox, Multiverse communications control protocol for electronic devices
-This program is free software; you can redistribute it And/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-*/
+
  int swi = 0;
  int swi2 = 0;
  int longcyc = 2;
@@ -43,10 +37,12 @@ void QuantumState() {
    //in a fraction of the time, this function isn't classically called, yet the potential 
    //of it being called is enough for it to be able to send information forward And back along the time axis.
    //this is the string which is supposed to be sent via quantum disruptions through time And the multiverse
- 
+   if (Serial.available()) {
+    integer = Serial.read();
+  }
    //supposed to implement crc32 to be able to verify the results once recieved...
 //   app.ShowPopup(integer);
-   if (integer <= ghostprotocol * range) {
+   if (integer < ghostprotocol * range) {
      for (int a = 0; a < 5; a++) {
        int b = 9;
        numa += b; //quantum disruption
@@ -54,7 +50,7 @@ void QuantumState() {
    }
  }
 void setup() {
-  randomSeed(analogRead(0));
+   randomSeed(analogRead(0));
 pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), QuantumState, HIGH);
  Serial.begin(9600);
@@ -62,20 +58,20 @@ pinMode(interruptPin, INPUT_PULLUP);
    int b = random(0, 2);
    numa += b; //initialise test variable set
  }
+ //Serial.println("Qubox C#, Arduino, Copyright 2019 George Wagenknecht");
+ 
 }
 
 void loop() {
  //various switching
 
  //GATHER QUANTUM VARIABLES
- char data[] = {random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20)};
- char data2[] = {random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20),random(0, 20)};
+ int data[] = {random(0, 20),random(0, 20),random(0, 20)};
+ int data2[] = {random(0, 20),random(0, 20),random(0, 20)};
   
-     if (Serial.available()) {
-    int integer = Serial.read();
-  }
+ 
    
-   if (Do == 1) {
+   if (Do == 1) { 
      Do2 = 1;
      if (qu == 1 && it == 0) {
        qu = 0;
@@ -87,27 +83,19 @@ void loop() {
      }
      it = 0;
    }
+ //more switching, this time, "test" alternates of each run of the camera data stream
+    
 
-   
-   if (test == 0) { //more switching, this time, "test" alternates of each run of the camera data stream
-     b = 0;
-     bb = 0;
-      //make two sets
-     test++;
-   }
-   if (test == 1) {
-     test = 0;
-     
-     for (int a = 0; a < 20; a++) {
+     for (int a = 0; a < 2; a++) {
        if (data[a] > 10) { //searching for motion intensity of data stream set
          b++; //counting initial spectra
         
        }
      }
      
-       for (int aa = 0; aa < 20; aa++) { //searching set two
+       for (int aa = 0; aa < 2; aa++) { //searching set two
          if (data[aa] > 10) {
-           if (data2[aa] > data[aa] || data2[aa] < data[aa]) { //trying to determine or differentiate quantum fluxuations of intensity 
+           if (data2[aa] > data[aa] || data2[aa] < data[aa]) { 
              bb++; //counting them
             
            }
@@ -121,10 +109,10 @@ void loop() {
      //^^^^ work on it...
    
      if (qu == 0) {
-       if (b > 4 && b < 11 || bb > 4 && bb < 11) { //comparing gathered information from each set, identifying causallity of entangled states, checking for the existence of wave function collapse
+       if (b > 1 && b < 11 || bb > 1 && bb < 11) { 
          Or++;
        }
-       if (b > 4 && b < 11 && bb > 4 && bb < 11) { //comparing gathered information from each set, identifying causallity of entangled states(laser moves solidly past the filter, no anomaly)
+       if (b > 1 && b < 11 && bb > 1 && bb < 11) { 
          And++;
          if (Do == 1) {
            if (qu == 1 && it == 0) {
@@ -140,9 +128,8 @@ void loop() {
          done = 1;
        }
      }
-     //same thing except this time we try to alter the flow by making different yet symmetrical choices whilst maintaning a quantum protocol, the protocol occurs within all switches, even the next ones after the whole function repeats(at the beginning)
      if (qu == 1) {
-       if (b > 4 && b < 11 || bb > 4 && bb < 11) {
+       if (b > 1 && b < 11 || bb > 1 && bb < 11) {
          if (Do2 == 1) {
            if (qu == 1 && it == 0) {
              qu = 0;
@@ -158,27 +145,23 @@ void loop() {
          }
        }
      }
-     if (b > 4 && b < 11 && bb > 4 && bb < 11) {
+     if (b > 1 && b < 11 && bb > 1 && bb < 11) {
        Or++;
        done = 1;
      }
     
-   }
 
-
+     b = 0;
+     bb = 0;
 
    if (And > Or) {
      //Serial.print(" And");
      //Serial.println(And);
      int check = numa[cyc];
      if (check != qu) {
-       if (swi == longcyc) { //long cycle quantum correction code, sooner or later you are going to want to alternate the protocol, to keep it fresh. the program probably won't work as well without it
-         qu = random(0, 1); //quantum correction code... without this the whole thing will probably not work, you could change it to a one or a zero
-         //and it will fail or freeze up. you may think how could a statically defined variable based on computer systems ever give rise to the kind of power quantum computers do
-         //random numbers are based on time and the exact time the function is called, this is a computer, it runs it's code in a ordered and physics based way, reliably
-         //so when you change the time the function is activated you change the output, what about the rest of the program? it's all just mechanical switches...
-         //why don't you try adding in a random delay for this random number generator and see if it affects the program.
-       }
+       if (swi == longcyc) { 
+         qu = random(0, 1); 
+         }
        swi++;
        Do = 1;
        nul++;
@@ -199,7 +182,7 @@ void loop() {
          it++;
        }
        it = 0;
-       if (swi2 == longcyc2) { //another long cycle quantum correction code, integrated with the quantum protocol
+       if (swi2 == longcyc2) { 
          qu = random(0, 1);
          swi2 = 0;
        }
@@ -240,7 +223,7 @@ void loop() {
        prime = 0;
      }
      check = numa[cyc];
-     if (check != qu) { //this section checks for disruptions in quantum flux, it is the time the code is most sure of inference/disruption(unless you turn the device off),why? because we cause the disruption at a future time by adding 9s to 'numa',you may wonder many things, you may aswell throw computer logic out the window because this is essentially completely made up code from here on
+     if (check != qu) { 
        if (swi == longcyc) {
          qu = random(0, 1);
          swi = 0;
@@ -265,6 +248,7 @@ void loop() {
            Serial.println(output);
          }
          if (calc == ghostprotocollast + range) {
+           output = ghostprotocol;
            ghostprotocollast = ghostprotocol;
            ghostprotocol = 0;
            GhostIterate = 0;
